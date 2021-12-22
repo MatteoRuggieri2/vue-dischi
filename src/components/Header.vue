b065941a9f81e86e5b50d8bc2529c40ed9760a6a
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-22 15:06:18 +0100

    Aggiunto header

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <div class="logo-container">
+            <img src="../assets/img/spotify-logo.png" alt="Spotify Logo">
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+    header {
+        background-color: #2e3a46;
+        height: 50px;
+        display: flex;
+        align-items: center;
+
+        .logo-container {
+            width: 30px;
+            margin: 0 20px;
+        }
+    }
+
+</style>
